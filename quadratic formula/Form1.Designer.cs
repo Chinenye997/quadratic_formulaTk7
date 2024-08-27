3f0610a4321750c8ffddbb2259cdca60bdf659c5
chinenye997 <edehg997@gmail.com>
2024-08-27 01:17:52 +0100

    quadratic formula task7

---
+++ quadratic formula/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace quadratic_formula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_a = new System.Windows.Forms.TextBox();
+            this.tb_b = new System.Windows.Forms.TextBox();
+            this.tb_c = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb_Answer2 = new System.Windows.Forms.Label();
+            this.lb_Answer1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_a
+            // 
+            this.tb_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_a.Location = new System.Drawing.Point(67, 85);
+            this.tb_a.Name = "tb_a";
+            this.tb_a.Size = new System.Drawing.Size(131, 53);
+            this.tb_a.TabIndex = 1;
+            // 
+            // tb_b
+            // 
+            this.tb_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_b.Location = new System.Drawing.Point(67, 204);
+            this.tb_b.Name = "tb_b";
+            this.tb_b.Size = new System.Drawing.Size(131, 53);
+            this.tb_b.TabIndex = 2;
+            // 
+            // tb_c
+            // 
+            this.tb_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_c.Location = new System.Drawing.Point(67, 323);
+            this.tb_c.Name = "tb_c";
+            this.tb_c.Size = new System.Drawing.Size(131, 53);
+            this.tb_c.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(290, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(253, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(253, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 37);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(362, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 46);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "(a)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(362, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 46);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "(b)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(362, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 46);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "(c)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(31, 521);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 32);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Answer1:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(31, 607);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(133, 32);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Answer2:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb_Answer2
+            // 
+            this.lb_Answer2.AutoSize = true;
+            this.lb_Answer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Answer2.Location = new System.Drawing.Point(350, 607);
+            this.lb_Answer2.Name = "lb_Answer2";
+            this.lb_Answer2.Size = new System.Drawing.Size(0, 46);
+            this.lb_Answer2.TabIndex = 13;
+            // 
+            // lb_Answer1
+            // 
+            this.lb_Answer1.AutoSize = true;
+            this.lb_Answer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Answer1.Location = new System.Drawing.Point(350, 521);
+            this.lb_Answer1.Name = "lb_Answer1";
+            this.lb_Answer1.Size = new System.Drawing.Size(0, 46);
+            this.lb_Answer1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(577, 689);
+            this.Controls.Add(this.lb_Answer1);
+            this.Controls.Add(this.lb_Answer2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_a);
+            this.Controls.Add(this.tb_c);
+            this.Controls.Add(this.tb_b);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "QUADRATIC FORMULA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_a;
+        private System.Windows.Forms.TextBox tb_b;
+        private System.Windows.Forms.TextBox tb_c;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lb_Answer2;
+        private System.Windows.Forms.Label lb_Answer1;
+    }
+}
+
---
+++ quadratic formula/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quadratic_formula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, ans1, ans2;
+
+            a = double.Parse(tb_a.Text);
+            b = double.Parse(tb_b.Text);
+            c = double.Parse(tb_c.Text);
+
+            ans1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+            ans2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+
+            lb_Answer1.Text = ans1.ToString("n2");
+            lb_Answer2.Text = ans2.ToString("n2");
+
+        }
+    }
+}
