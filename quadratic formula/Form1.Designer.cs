46c237e6eb09de5ce7aa7fd88ab294e7c2935754
chinenye997 <edehg997@gmail.com>
2025-01-16 12:15:26 +0100

    I repeated the task after making the necessary changes

--- quadratic formula/Form1.Designer.cs
+++ quadratic formula/Form1.Designer.cs
@@ -37,17 +37,19 @@ namespace quadratic_formula
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.lb_Answer2 = new System.Windows.Forms.Label();
             this.lb_Answer1 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnShowFormula = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tb_a
             // 
             this.tb_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_a.Location = new System.Drawing.Point(67, 85);
+            this.tb_a.Location = new System.Drawing.Point(330, 104);
             this.tb_a.Name = "tb_a";
             this.tb_a.Size = new System.Drawing.Size(131, 53);
             this.tb_a.TabIndex = 1;
@@ -55,7 +57,7 @@ namespace quadratic_formula
             // tb_b
             // 
             this.tb_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_b.Location = new System.Drawing.Point(67, 204);
+            this.tb_b.Location = new System.Drawing.Point(329, 204);
             this.tb_b.Name = "tb_b";
             this.tb_b.Size = new System.Drawing.Size(131, 53);
             this.tb_b.TabIndex = 2;
@@ -63,7 +65,7 @@ namespace quadratic_formula
             // tb_c
             // 
             this.tb_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_c.Location = new System.Drawing.Point(67, 323);
+            this.tb_c.Location = new System.Drawing.Point(331, 302);
             this.tb_c.Name = "tb_c";
             this.tb_c.Size = new System.Drawing.Size(131, 53);
             this.tb_c.TabIndex = 3;
@@ -72,7 +74,7 @@ namespace quadratic_formula
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(338, 87);
+            this.label4.Location = new System.Drawing.Point(33, 106);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(269, 46);
             this.label4.TabIndex = 7;
@@ -82,7 +84,7 @@ namespace quadratic_formula
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(338, 204);
+            this.label5.Location = new System.Drawing.Point(33, 208);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(269, 46);
             this.label5.TabIndex = 8;
@@ -92,7 +94,7 @@ namespace quadratic_formula
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(338, 335);
+            this.label6.Location = new System.Drawing.Point(38, 307);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(268, 46);
             this.label6.TabIndex = 9;
@@ -102,7 +104,7 @@ namespace quadratic_formula
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(31, 521);
+            this.label7.Location = new System.Drawing.Point(31, 482);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(133, 32);
             this.label7.TabIndex = 10;
@@ -112,27 +114,27 @@ namespace quadratic_formula
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(31, 607);
+            this.label8.Location = new System.Drawing.Point(31, 556);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(133, 32);
             this.label8.TabIndex = 11;
             this.label8.Text = "Answer2:";
             // 
-            // button1
+            // btnCalculate
             // 
-            this.button1.Location = new System.Drawing.Point(37, 431);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 48);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCalculate.Location = new System.Drawing.Point(37, 402);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(127, 46);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Enter";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
             // 
             // lb_Answer2
             // 
             this.lb_Answer2.AutoSize = true;
             this.lb_Answer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_Answer2.Location = new System.Drawing.Point(350, 607);
+            this.lb_Answer2.Location = new System.Drawing.Point(350, 560);
             this.lb_Answer2.Name = "lb_Answer2";
             this.lb_Answer2.Size = new System.Drawing.Size(0, 46);
             this.lb_Answer2.TabIndex = 13;
@@ -141,7 +143,7 @@ namespace quadratic_formula
             // 
             this.lb_Answer1.AutoSize = true;
             this.lb_Answer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_Answer1.Location = new System.Drawing.Point(350, 521);
+            this.lb_Answer1.Location = new System.Drawing.Point(350, 474);
             this.lb_Answer1.Name = "lb_Answer1";
             this.lb_Answer1.Size = new System.Drawing.Size(0, 46);
             this.lb_Answer1.TabIndex = 14;
@@ -150,7 +152,7 @@ namespace quadratic_formula
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(290, 63);
+            this.label1.Location = new System.Drawing.Point(280, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 22);
             this.label1.TabIndex = 4;
@@ -159,20 +161,44 @@ namespace quadratic_formula
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(253, 85);
+            this.label2.Location = new System.Drawing.Point(243, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 37);
             this.label2.TabIndex = 5;
+            // 
+            // btnShowFormula
+            // 
+            this.btnShowFormula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowFormula.Location = new System.Drawing.Point(35, 30);
+            this.btnShowFormula.Name = "btnShowFormula";
+            this.btnShowFormula.Size = new System.Drawing.Size(253, 34);
+            this.btnShowFormula.TabIndex = 15;
+            this.btnShowFormula.Text = "Show Formula\r\n\r\n";
+            this.btnShowFormula.UseVisualStyleBackColor = true;
+            this.btnShowFormula.Click += new System.EventHandler(this.btnShowFormula_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnClear.Location = new System.Drawing.Point(608, 560);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 37);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gray;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(731, 661);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnShowFormula);
             this.Controls.Add(this.lb_Answer1);
             this.Controls.Add(this.lb_Answer2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -201,11 +227,13 @@ namespace quadratic_formula
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Label lb_Answer2;
         private System.Windows.Forms.Label lb_Answer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShowFormula;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- quadratic formula/Form1.cs
+++ quadratic formula/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace quadratic_formula
 {
     public partial class Form1 : Form
@@ -19,22 +21,90 @@ namespace quadratic_formula
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a, b, c, ans1, ans2;
+            MessageBox.Show("Clicked!"); // Button to click
+            
 
-            a = double.Parse(tb_a.Text);
-            b = double.Parse(tb_b.Text);
-            c = double.Parse(tb_c.Text);
+            try
+            {
+                // Parse inputs
+                double a = double.Parse(tb_a.Text);
+                double b = double.Parse(tb_b.Text);
+                double c = double.Parse(tb_c.Text);
 
-            ans1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
-            ans2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+                // Check if a is zero
+                if (a == 0)
+                {
+                    MessageBox.Show("Coefficient 'a' cannot be 0 for a quadratic equation.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
-            lb_Answer1.Text = ans1.ToString("n2");
-            lb_Answer2.Text = ans2.ToString("n2");
+                // Calculate discriminant
+                double discriminant = Math.Pow(b, 2) - (4 * a * c);
+
+                if (discriminant > 0)
+                {
+                    // Two distinct real roots
+                    double ans1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    double ans2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+                    lb_Answer1.Text = ans1.ToString("n2");
+                    lb_Answer2.Text = ans2.ToString("n2");
+                }
+                else if (discriminant == 0)
+                {
+                    // One real root (repeated)
+                    double ans = -b / (2 * a);
+
+                    lb_Answer1.Text = ans.ToString("n2");
+                    lb_Answer2.Text = "Repeated root";
+                }
+                else
+                {
+                    // Complex roots
+                    double realPart = -b / (2 * a);
+                    double imaginaryPart = Math.Sqrt(-discriminant) / (2 * a);
+
+                    lb_Answer1.Text = $"{realPart:n2} + {imaginaryPart:n2}i";
+                    lb_Answer2.Text = $"{realPart:n2} - {imaginaryPart:n2}i";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numeric values for a, b, and c.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
 
         }
 
-       
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear input fields
+            tb_a.Text = string.Empty;
+            tb_b.Text = string.Empty;
+            tb_c.Text = string.Empty;
 
-       
+
+            // Clear output labels
+            lb_Answer1.Text = string.Empty;
+            lb_Answer2.Text = string.Empty;
+        }
+
+        private void btnShowFormula_Click(object sender, EventArgs e)
+        {
+
+            // Check the current text of the button
+            if (btnShowFormula.Text == "Show Formula")
+            {
+                // Change the text to display the formula
+                btnShowFormula.Text = "x = (-b ± √(b² - 4ac)) / 2a";
+            }
+            else
+            {
+                // Change the text back to the original
+                btnShowFormula.Text = "Show Formula";
+            }
+
+
+        }
     }
 }
+
